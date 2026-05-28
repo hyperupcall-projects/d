955655dfd379c3cf664c18e9ce4701eed8eb7991
Edwin Kofler <edwin@kofler.dev>
2026-05-28 14:07:30 -0700

    Restructure to use defer to cleanup resources

--- d.c
+++ d.c
@@ -19,6 +19,32 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+// Defer macros by Jens Gustedt
+// https://gustedt.wordpress.com/2026/02/15/defer-available-in-gcc-and-clang/
+// clang-format off
+#if __has_include(<stddefer.h>)
+# include <stddefer.h>
+# if defined(__clang__)
+#  if __is_identifier(_Defer)
+#   error "clang may need the option -fdefer-ts for the _Defer feature"
+#  endif
+# endif
+#elif __GNUC__ > 8
+# define defer _Defer
+# define _Defer      _Defer_A(__COUNTER__)
+# define _Defer_A(N) _Defer_B(N)
+# define _Defer_B(N) _Defer_C(_Defer_func_ ## N, _Defer_var_ ## N)
+# define _Defer_C(F, V)                                                \
+  auto void F(int*);                                                   \
+  __attribute__((__cleanup__(F), __deprecated__, __unused__))          \
+     int V;                                                            \
+  __attribute__((__always_inline__, __deprecated__, __unused__))       \
+    inline auto void F(__attribute__((__unused__)) int*V)
+#else
+# error "The _Defer feature does not seem to be available"
+#endif
+// clang-format on
+
 void error(const char *message) {
 #define RED "\033[0;31m"
 #define RESET "\033[0m"
@@ -34,14 +60,6 @@ void deploy(char *, char *, bool, bool);
 int main(int argc, char *argv[]) {
 	static bool is_debug = false;
 	static bool is_dry_run = false;
-
-	char *config_file = NULL;
-	char *config_file3 = NULL;
-	char *config_filename = NULL;
-	char *cache_dir = NULL;
-	char *so_file = NULL;
-	char *deployment_name = NULL;
-	void *handle = NULL;
 
 	if (getenv("DEBUG") != NULL) {
 		is_debug = true;
@@ -73,6 +91,7 @@ int main(int argc, char *argv[]) {
 		fail("Failed to recognize subcommand");
 	}
 
+	char *deployment_name = NULL;
 	if ((command == CommandDeploy || command == CommandUndeploy) && argc > 2) {
 		for (int i = 2; i < argc; i += 1) {
 			if (strcmp(argv[i], "--dry") == 0) {
@@ -85,129 +104,147 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	config_file = strdup(CONFIG_FILE);
+	char *config_file = strdup(CONFIG_FILE);
 	if (config_file == NULL) {
 		error("Failed to copy config path");
-		goto error;
+		return EXIT_FAILURE;
 	}
-	config_file3 = strdup(config_file);
+	defer {
+		free(config_file);
+	};
+
+	char *config_file3 = strdup(config_file);
 	if (config_file3 == NULL) {
 		error("Failed to copy config path");
-		goto error;
+		return EXIT_FAILURE;
 	}
-	config_filename = basename(config_file3);
+	defer {
+		free(config_file3);
+	};
+
+	char *config_filename = basename(config_file3);
 
 	struct stat st;
 	if (stat(config_file, &st) == -1) {
 		perror("Failed to stat file");
-		goto error;
+		return EXIT_FAILURE;
 	}
 	if (S_ISDIR(st.st_mode)) {
 		error("Config file must not be a directory");
-		goto error;
+		return EXIT_FAILURE;
 	}
 
 	const char *home = getenv("HOME");
 	if (home == NULL) {
 		error("HOME is not set");
-		goto error;
+		return EXIT_FAILURE;
 	}
 	const char *xdg_cache = getenv("XDG_CACHE_HOME");
+	char *cache_dir;
 	if (xdg_cache != NULL && xdg_cache[0] != '\0') {
 		cache_dir = strdup(xdg_cache);
 		if (cache_dir == NULL) {
 			error("Failed to copy XDG_CACHE_HOME");
-			goto error;
+			return EXIT_FAILURE;
 		}
 	} else {
 		if (asprintf(&cache_dir, "%s/.cache", home) == -1) {
 			error("Failed to create cache dir path");
-			goto error;
+			return EXIT_FAILURE;
 		}
 	}
+	defer {
+		free(cache_dir);
+	};
+
 	if (mkdir(cache_dir, 0700) == -1 && errno != EEXIST) {
 		perror("Failed to create cache directory");
-		goto error;
+		return EXIT_FAILURE;
 	}
 
+	char *so_file;
 	if (asprintf(&so_file, "%s/libdotfiles.so", cache_dir) == -1) {
 		error("Failed to create library path");
-		goto error;
+		return EXIT_FAILURE;
 	}
+	defer {
+		free(so_file);
+	};
 
-	char *obj_file = NULL;
+	char *obj_file;
 	if (asprintf(&obj_file, "%s/%s.o", cache_dir, config_filename) == -1) {
 		error("Failed to create object file path");
-		goto error;
+		return EXIT_FAILURE;
 	}
+	defer {
+		free(obj_file);
+	};
 
 	{
-		char *config_home = NULL;
+		char *config_home;
 		if (asprintf(&config_home, "-DCONFIG_HOME=\"%s\"", home) == -1) {
 			error("Failed to create CONFIG_HOME define");
-			free(obj_file);
-			goto error;
+			return EXIT_FAILURE;
 		}
+		defer {
+			free(config_home);
+		};
 
 		char *const compile_argv[] = {"gcc", "-g", "-fPIC", "-c", config_file, config_home, "-o", obj_file, NULL};
 		pid_t pid;
 		if (posix_spawnp(&pid, "gcc", NULL, NULL, compile_argv, environ) != 0) {
 			perror("gcc");
-			free(obj_file);
-			free(config_home);
-			goto error;
+			return EXIT_FAILURE;
 		}
 		int status;
 		if (waitpid(pid, &status, 0) == -1 || !WIFEXITED(status) || WEXITSTATUS(status) != 0) {
 			error("Compilation failed");
-			free(obj_file);
-			free(config_home);
-			goto error;
+			return EXIT_FAILURE;
 		}
-		free(config_home);
 	}
 	{
 		char *const link_argv[] = {"gcc", "-shared", "-o", so_file, obj_file, NULL};
 		pid_t pid;
 		if (posix_spawnp(&pid, "gcc", NULL, NULL, link_argv, environ) != 0) {
 			perror("gcc");
-			free(obj_file);
-			goto error;
+			return EXIT_FAILURE;
 		}
 		int status;
 		if (waitpid(pid, &status, 0) == -1 || !WIFEXITED(status) || WEXITSTATUS(status) != 0) {
 			error("Linking failed");
-			free(obj_file);
-			goto error;
+			return EXIT_FAILURE;
 		}
 	}
-	free(obj_file);
 
-	handle = dlopen(so_file, RTLD_LAZY);
+	void *handle = dlopen(so_file, RTLD_LAZY);
 	if (handle == NULL) {
 		fprintf(stderr, "%s\n", dlerror());
-		goto error;
+		return EXIT_FAILURE;
 	}
+	defer {
+		dlclose(handle);
+	};
+
 	dlerror();
 
 	Deployment **(*getDeployments)(void) = (Deployment * *(*)(void)) dlsym(handle, "getDeployments");
 	char *dl_error = dlerror();
 	if (dl_error != NULL) {
 		fprintf(stderr, "%s\n", dl_error);
-		goto error;
+		return EXIT_FAILURE;
 	}
 
 	Deployment *(*getDefaultDeployment)(void) = (Deployment * (*)(void)) dlsym(handle, "getDefaultDeployment");
 	dl_error = dlerror();
 	if (dl_error != NULL) {
 		fprintf(stderr, "%s\n", dl_error);
-		goto error;
+		return EXIT_FAILURE;
 	}
 
 	Deployment **deployments = getDeployments();
 	if (deployments == NULL) {
 		fprintf(stderr, "Failed to get deployments\n");
-		goto error;
+		return EXIT_FAILURE;
 	}
 
 	Deployment *current_deployment = NULL;
@@ -222,13 +259,13 @@ int main(int argc, char *argv[]) {
 		}
 		if (current_deployment == NULL) {
 			fprintf(stderr, "Deployment '%s' not found\n", deployment_name);
-			goto error;
+			return EXIT_FAILURE;
 		}
 	}
 
 	if (current_deployment->items == NULL) {
 		fprintf(stderr, "Failed to get configuration\n");
-		goto error;
+		return EXIT_FAILURE;
 	}
 
 	if (command == CommandPrint)
@@ -279,7 +316,7 @@ int main(int argc, char *argv[]) {
 						if (errno != ENOENT) {
 							fprintf(stderr, "Failed to unlink \"%s\"\n", destination_path);
 							perror("unlink");
-							goto error;
+							return EXIT_FAILURE;
 						}
 					}
 				}
@@ -289,19 +326,7 @@ int main(int argc, char *argv[]) {
 	if (command == CommandPrint)
 		printf("]\n");
 
-error:
-	if (handle)
-		dlclose(handle);
-	if (cache_dir)
-		free(cache_dir);
-	if (so_file)
-		free(so_file);
-	if (config_file)
-		free(config_file);
-	if (config_file3)
-		free(config_file3);
-
-	exit(1);
+	return EXIT_SUCCESS;
 }
 
 void deploy(char *source_path, char *destination_path, bool debug, bool dry_run) {
@@ -349,16 +374,15 @@ void deploy(char *source_path, char *destination_path, bool debug, bool dry_run)
 	{
 		char *dir = malloc(strlen(destination_path) + 1);
 		if (dir == NULL) {
-			error("Failed to allocate path memory");
-			goto error;
+			fail("Failed to allocate path memory");
 		}
+		defer {
+			free(dir);
+		};
 
 		strcpy(dir, destination_path);
 		dirname(dir);
-		if (dir == NULL) {
-			error("Failed to extract directory name");
-			goto error;
-		}
+
 		struct stat st = {0};
 		if (stat(dir, &st) == -1) {
 			if (errno != ENOENT) {
@@ -387,13 +411,6 @@ void deploy(char *source_path, char *destination_path, bool debug, bool dry_run)
 				}
 			}
 		}
-		free(dir);
-
-		goto end;
-	error:
-		free(dir);
-		exit(1);
-	end:;
 	}
 
 	struct stat st = {0};
@@ -445,28 +462,27 @@ void deploy(char *source_path, char *destination_path, bool debug, bool dry_run)
 			if (source_fd == -1) {
 				fail("Failed to open source file");
 			}
+			defer {
+				close(source_fd);
+			};
 
 			int dest_fd = open(destination_path, O_WRONLY | O_CREAT, 0644);
 			if (dest_fd == -1) {
-				close(source_fd);
 				fail("Failed to create destination file");
 			}
+			defer {
+				close(dest_fd);
+			};
 
 			struct stat stat_buf;
 			if (fstat(source_fd, &stat_buf) == -1) {
-				close(source_fd);
-				close(dest_fd);
 				fail("Failed to get file size");
 			}
 
 			if (sendfile(dest_fd, source_fd, NULL, stat_buf.st_size) == -1) {
-				close(source_fd);
-				close(dest_fd);
 				fail("Failed to copy file");
 			}
 
-			close(source_fd);
-			close(dest_fd);
 			printf("Copied %s to %s\n", source_path, destination_path);
 		}
 	}
